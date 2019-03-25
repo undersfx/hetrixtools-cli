111467d5ec7656f0d29410291a5a53fc1e13606e
undersfx <atchin_@hotmail.com>
2019-03-24 22:18:11 -0300

    loggin

--- main.py
+++ main.py
@@ -31,24 +31,24 @@ while True:
 
 # Chama Selenium para Delistar IPs
 
-browser = webdriver.Chrome()
-
-for ip in blacklisted:
-	#browser.get('https://dnsrbl.org/remove.cgi?ip=177.70.232.115')
-	browser.get('https://dnsrbl.org/lookup.cgi?ip={}'.format(ip))
-	print('Delisting: {}'.format(ip))
+if blacklisted:
+	browser = webdriver.Chrome()
 	
-	try:
-		elem = browser.find_element_by_css_selector('#gobutton')
-		elem.click()
-	except Exception as e:
-		print('Erro: {}'.format(e))
-		raise e
-	else:
-		print('{} Delisted.'.format(ip))
+	for ip in blacklisted:
+		#browser.get('https://dnsrbl.org/remove.cgi?ip=177.70.232.115')
+		browser.get('https://dnsrbl.org/lookup.cgi?ip={}'.format(ip))
+		print('Delisting: {}'.format(ip))
+		
+		try:
+			elem = browser.find_element_by_css_selector('#gobutton')
+			elem.click()
+		except Exception as e:
+			print('Erro: {}'.format(e))
+			raise e
+		else:
+			print('{} Delisted.'.format(ip))
 
-
-print('all processes completed successfully')
+print('Finished!')
 
 # DEBUG:
 # print(blacklisted)
