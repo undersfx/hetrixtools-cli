2de2ba6b2d109f6da009902190acecc3db698ab3
undersfx <atchin_@hotmail.com>
2019-04-26 01:18:40 -0300

    adding gitignore

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ blacklisted = []
 
 while True:
 	print('Parsing JSON data. Size: {}'.format(len(json_data[0])))
+	# json_data[0] contém a lista com dicts para cada IP
+	# json_data[1] comtém um dict de referéncia para o próximo 
 	for ip in json_data[0]:
 		if ip['Blacklisted_Count'] != '0':
 			for rbl in ip['Blacklisted_On']:
@@ -29,13 +31,15 @@ while True:
 	except KeyError:
 		break
 
+print('Total listed IPs found: {}'.format(len(blacklisted)))
+
 # Chama Selenium para Delistar IPs
 
 if blacklisted:
 	browser = webdriver.Chrome()
 	
 	for ip in blacklisted:
-		#browser.get('https://dnsrbl.org/remove.cgi?ip=177.70.232.115')
+		# browser.get('https://dnsrbl.org/remove.cgi?ip=177.70.232.115')
 		browser.get('https://dnsrbl.org/lookup.cgi?ip={}'.format(ip))
 		print('Delisting: {}'.format(ip))
 		
@@ -48,47 +52,6 @@ if blacklisted:
 		else:
 			print('{} Delisted.'.format(ip))
 
+browser.close()
+
 print('Finished!')
-
-# DEBUG:
-# print(blacklisted)
-# print(len(blacklisted))
-
-'''
-json_data[0] contém a lista com dicts para cada IP
-json_data[1] comtém um dict de referéncia para o próximo 
-
-Exemplo de Retorno:
-
-[
-	[
-		{
-			"ID": "da32f23a389794668b03b21261fb4428",
-			"Type": "IPv4",
-			"Target": "177.70.224.0",
-			"Add_Date": 1519929211,
-			"Last_Check": 1553140206,
-			"Status": "Active",
-			"Label": "",
-			"Contact_List_ID": "ddb7b88b880072537785efd068370f15",
-			"Blacklisted_Count": "0",
-			"Blacklisted_On": null,
-			"Links": {
-				"Report_Link": "https://hetrixtools.com/report/blacklist/b5cf91a559d71711428fa46d47b4225a/",
-				"Whitelabel_Report_Link": "http://rbl.allin.com.br/report/blacklist/b5cf91a559d71711428fa46d47b4225a/"
-			}
-		}
-	],
-	{
-		"Meta": {
-			"Total_Records": "24576"
-		},
-		"Links": {
-			"Pages": {
-				"Next": "https://api.hetrixtools.com/v2/a8b6c925e6d01613deaf5f5c48581f8f/blacklist/monitors/1/1/"
-			}
-		}
-	}
-]
-
-'''
