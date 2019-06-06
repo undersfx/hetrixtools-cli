b265645651fffa6add5f50fdb02dba712b78c9c6
undersfx <atchin_@hotmail.com>
2019-06-06 19:11:39 -0300

    added argparse

--- main.py
+++ main.py
@@ -1,33 +1,66 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser
 from selenium import webdriver
+from pprint import pprint
 import blacklist
 import monitors
 import delist
 import json
-import pprint
 
-try: 
-    # Tenta usar arquivo de cache
-    with open('cached_data.json', 'r') as f:
-        print('Cached data found, using the file.')
-        ips = json.load(f)
-except FileNotFoundError:
-    # Buscar ips bloqueados
-    print('Cached data not found, updating.')
-    ips = monitors.get_data()
+# Parser config
+parser = ArgumentParser(
+    prog='rbl',
+    description='Tools to maintain my sanity',)
 
-# Salva novo cache
-with open('cached_data.json', 'w') as f:
-    json.dump(ips, f)
+parser.add_argument('-u', action='store_true',
+                    help='update cache file')
 
-# Printa o resumo de blacklists
-bl_count = blacklist.count(ips)
+parser.add_argument('-c', action='store_true',
+                    help='count blacklisted ips')
 
-pprint.pprint(sorted(bl_count.items(), key=lambda x: x[1], reverse=True))
+parser.add_argument('-l', action='store_true',
+                    help='list all ips blacklisted and its blacklist') # TODO
 
-# # Identifica quais IPs estão listados em 'dnsrbl.org'
-# blacklisted = blacklist.find_blocked(ips, 'dnsrbl.org')
+parser.add_argument('--dnsrbl', action='store_true',
+                    help='run the delist process for dnsrbl.org')
 
-# # Chama Selenium para Delistar IPs
-# if blacklisted: delist.dnsrbl(blacklisted)
+def main():
+    args = parser.parse_args()
 
-print('Finished!')
+    def load_cache():
+        with open('cached_data.json', 'r') as f:
+            ips = json.load(f)
+            print('Cached data found, using the file.')
+            return ips
+
+    def update_cache(ips):
+        with open('cached_data.json', 'w') as f:
+            json.dump(ips, f)
+            print('New cache saved')
+
+    if args.u:
+        ips = monitors.get_data()
+        update_cache(ips)
+    else:
+        try:
+            ips = load_cache()
+        except FileNotFoundError:
+            ips = monitors.get_data()
+            update_cache(ips)
+
+    # Print the blacklist count
+    if args.c:
+        bl_count = blacklist.count(ips)
+        pprint(sorted(bl_count.items(), key=lambda x: x[1], reverse=True))
+
+    if args.dnsrbl:
+        # Identifica quais IPs estão listados em 'dnsrbl.org'
+        blacklisted = blacklist.find_blocked(ips, 'dnsrbl.org')
+        # Chama Selenium para Delistar IPs
+        if blacklisted: delist.dnsrbl(blacklisted)
+
+    print('Finished!')
+
+if __name__ == "__main__":
+    main()
